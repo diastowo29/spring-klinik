fcd47964c2e0b57a8389d589c8e344f72f82bb09
diastowo <diastowo@gmail.com>
2020-01-27 17:41:09 +0700

    cleaning setupcontroller

--- src/main/java/org/springframework/samples/petclinic/setup/SetupController.java
+++ src/main/java/org/springframework/samples/petclinic/setup/SetupController.java
@@ -7,23 +7,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 class SetupController {
-
-	// private final VetRepository vets;
-	//
-	// public VetController(VetRepository clinicService) {
-	// this.vets = clinicService;
-	// }
-
+	
 	@GetMapping("/setup")
 	public String showSetup(Map<String, Object> model) {
 		return "setup/setup";
 	}
-
-	// @GetMapping({ "/vets" })
-	// public @ResponseBody Vets showResourcesVetList() {
-	// Vets vets = new Vets();
-	// vets.getVetList().addAll(this.vets.findAll());
-	// return vets;
-	// }
 
 }
